626ca7a07aaf3b6a55d3de79e820c03968a36800
fz3002 <franc3002@gmail.com>
2024-03-12 21:21:04 +0100

    Finish knn function and interface as well as function that checks accurcy for every k from range 0 to 45. Some other small changes

--- MPP1/main.py
+++ MPP1/main.py
@@ -1,5 +1,7 @@
 import csv
+import random
 
+#read csv file into list
 def read_file(file_name):
     list = []
     with open(file_name, newline='') as f:
@@ -9,21 +11,69 @@ def read_file(file_name):
     f.close()
     return list
 
+#function finding k closest vectors to a given one
 def knn(k, vector, space):
     closest = []
+    most_frequent = {}
+    distance_vector_atribute = []
     for v in space:
-        closest.sort()
-        sum = 0;
-        for i in range(len(vector)-1):
-            sum += (vector[i] - v[i])**2
-        if(closest.length < k):
-            closest.append((sum, v[v.length-1]))
+        sum = 0.0;
+        for i in range(len(vector)):
+            sum += ((float(vector[i]) - float(v[i]))**2)
+        distance_vector_atribute.append((round(sum, 3), v[len(v) - 1]))
+    distance_vector_atribute.sort()
+    closest = distance_vector_atribute[:k]
+    
+    for e in closest:
+        if(e[1] not in list(most_frequent.keys())):
+            most_frequent.update({e[1]:1})
         else:
-            if(closest[closest.length-1][0] > sum):
-                closest[closest.length-1] = (sum, v[v.length-1])
-    #TODO: find every possible outcome and put it in array
-    #TODO: count number of outcomes in closest and return verdict
-                  
+            most_frequent[e[1]] += 1
+            
+    max_frequent = max(most_frequent.values())
+    
+    for e in list(most_frequent.keys()):
+        if (most_frequent[e] == max_frequent):
+            return e
+        
+#print out accuracy for k from range 1 to 45        
+def accuracy_for_each_k(test_list, train_list):
+    for i in range(1,46):
+        good_results = 0
+        for e in test_list:
+            vector = e[:-1]
+            test_result = e[-1]
+            knn_result = knn(i, vector, train_list)
+            if(knn_result == test_result):
+                good_results += 1
+        print("k:", i, "accuracy: ", good_results/len(test_list))     
+        
 k = int(input("Podaj k: "))
 train_list = read_file("train_set.csv")
 test_list = read_file("test_set.csv")
+random.shuffle(train_list)
+random.shuffle(test_list)
+good_results = 0
+number_of_coords = len(train_list[0])-1
+
+#testing on test_set.csv
+for e in test_list:
+    vector = e[:-1]
+    test_result = e[-1]
+    knn_result = knn(k, vector, train_list)
+    if(knn_result == test_result):
+        good_results += 1
+    print("result: ", knn_result, "from test_set: ", test_result)
+    
+print()
+#print out accuracy of testing for given k
+print("accuracy: ", good_results/len(test_list))
+
+accuracy_for_each_k(test_list, train_list)
+
+#Looped vector testing
+while(True):
+    input_vec = []
+    for i in range(number_of_coords):
+        input_vec.append(float(input("Podaj współrzędną: ")))
+    print(knn(k, input_vec, train_list))
