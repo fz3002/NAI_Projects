e8607f4fc5d5e0ed4b3311ea38bcc4c06b1ad19c
Franciszek Zych <franc3002@gmail.com>
2024-03-12 15:20:47 +0100

    Find k closest vectors

--- MPP1/main.py
+++ MPP1/main.py
@@ -9,6 +9,19 @@ def read_file(file_name):
     f.close()
     return list
 
+def knn(k, vector, space):
+    closest = []
+    for v in space:
+        closest.sort()
+        sum = 0;
+        for i in range(len(vector)-1):
+            sum += (vector[i] - v[i])**2
+        if(closest.length < k):
+            closest.append((sum, v[v.length-1]))
+        else:
+            if(closest[closest.length-1][0] > sum):
+                closest[closest.length-1] = (sum, v[v.length-1])
+                  
 k = int(input("Podaj k: "))
 train_list = read_file("train_set.csv")
 test_list = read_file("test_set.csv")
